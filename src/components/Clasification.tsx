523de7c2cce781802e194d6afd0e29d3a8601b37
daninavagar <daninavagar@gmail.com>
2023-11-14 20:52:57 +0100

    google_sheets

--- src/components/Clasification.tsx
+++ src/components/Clasification.tsx
@@ -1,7 +1,27 @@
 import { Image } from "@nextui-org/image";
 import clasificationImage from '../assets/img/clasification.png'
+import { useState, useEffect } from 'react'
+import { getValues } from "../services/sheet_get_values";
+
+
+
 function Clasification() {
 
+
+
+  const [clasification, setClasification] = useState()
+
+  useEffect(() => {
+
+    console.log('obteniendo información');
+    const spreadsheetId = '1r3uP2mEJuUwHVewI-FkR8J1fNe3UGE4A'
+    const range = 'Clasificación!A1:B10'
+    const rows = getValues(spreadsheetId, range)
+    
+    // console.log(rows);
+        
+
+  },[])
   return (
     <Image
       src={clasificationImage}
